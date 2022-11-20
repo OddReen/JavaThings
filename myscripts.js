8f086c40aff8e5ed5e347837b383364d4a494335
OddReen <114663295+OddReen@users.noreply.github.com>
2022-11-20 19:31:28 +0000

    JavaLessGooo

---
+++ myscripts.js
@@ -0,0 +1,35 @@
+let MyName = "Leonardo";
+let MyAge = 20;
+let MyMultipliedAge = Age * 7;
+
+console.log(MyName, MyAge, MyMultipliedAge);
+
+let MyFeatures = {
+    MyName: "Leonardo",
+    MyAge: 22,
+    isStudent: true
+};
+
+let CodeName = ["Calvo", "Namorada do Calvo", "Dora a Exploradora"];
+
+let WarTroops= 
+[
+    {
+    CodeName: "Calvo",
+    Age: 22,
+    isStudent: true,
+    isCringe: true
+},
+{
+    CodeName: "Namorada do Calvo",
+    Age: 7,
+    isStudent: true,
+    isCringe: true
+},
+{
+    CodeName: "Dora a exploradora",
+    Age: 8,
+    isStudent: false,
+    isCringe: false
+}
+];
